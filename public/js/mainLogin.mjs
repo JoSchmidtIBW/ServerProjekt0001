d42858ee766291361ec05f867cc8a56b3686d2fc
John <flnc85@hotmail.de>
2022-11-29 21:31:03 +0100

    Serverkommunikation lernen,...

--- public/js/mainLogin.mjs
+++ public/js/mainLogin.mjs
@@ -3,3 +3,23 @@ console.log("MainLogin.js hallllllllllllllllllllllllllllllllllllllllllllllllllll
 function myFunctionClicky(){
     console.log("halllllllllllllllllllllllllllllllllllllllllllllllllllllllll");
 }
+
+
+async function loadJoke() {
+    try { //hier wäre fehler status 400
+        const response = await fetch('https://api.chucknorris.io/jokes/random');
+        console.log(response);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data);
+            console.log(data.value);
+            document.getElementById('j').innerText = data.value;
+        } else {
+            console.error(await response.text);
+            document.getElementById('j').innerText = "Kein Internet!!!";
+        }
+    } catch (e) {
+        console.error(e);
+        document.getElementById('j').innerText = "Kein Internet!!!";
+    }
+}
